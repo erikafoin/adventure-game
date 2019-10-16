d4d36d69b9e18537986ac44a22fa1178c93f5b86
Erika Foin <erika.foin@gmail.com>
2019-10-16 16:28:12 -0700

    first commit

---
+++ adventure-data.js
@@ -0,0 +1,22 @@
+const adventures = {
+    id: 'bus',
+    title: 'Boarding the Bus'
+    image:
+    audio:
+    description: `
+    The bus pulls up and there are a bunch of people standing right by the door who could easily move back but aren't. What do you do?
+    `,
+    choices: [{
+        id: 'squeeze in',
+        description: 'Negotiate with them',
+        result: `
+            Knowing the monsters are not too bright, you offer to go buy them all
+            turkey dinners from the village pub. They give you 35 gold for meals
+            that will never be delivered. I hope you can live with yourself. 
+        `,
+        hp: 0,
+        gold: 35
+    }, {
+    }]
+
+}
