9eb32e81eb0559368d6606f299e7bf57b73824d9
Erika Foin <erika.foin@gmail.com>
2019-10-19 13:05:59 -0700

    saturday afternoon coding

--- common/load-profile.js
+++ common/load-profile.js
@@ -1,30 +1,22 @@
-import { getAlly } from '../data/api.js';
-import gameOver from '/common/game-over.js';
-
+import { getAlly } from './saveAlly.js';
+import gameOver from './game-over.js';
 function loadProfile() {
     const name = document.getElementById('name');
     const avatar = document.getElementById('avatar');
     const wp = document.getElementById('wp');
     const sc = document.getElementById('sc');
-
     const ally = getAlly();
-
     if (!ally) {
         window.location = './index.html';
     }
-
     name.textContent = ally.name;
-    avatar.src = '../assets/avatars/' + ally.animal + '.png';
+    avatar.src = './assets/avatar' + ally.animal + '.png';
     sc.textContent = ally.sc;
-
     if (gameOver(ally)) {
         wp.textContent = 'Better Try Again, my friend';
     }
     else {
-
         wp.textContent = ally.wp;
     }
-
 }
-
 export default loadProfile;
